0c5479869cecde68f127c650e8529f0b2e1ffb13
Alexander Flores <alexflores09.06.90@gmail.com>
2017-11-14 21:12:26 -0600

    Se agregar los iconos para el formulario de preguntas

--- src/app/question/question-form.component.ts
+++ src/app/question/question-form.component.ts
@@ -1,18 +1,40 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Question } from './question.model';
+import icons from './icons';
 
 @Component({
   selector: 'app-question-form',
-  templateUrl: './question-form.component.html'
+  templateUrl: './question-form.component.html',
+  styles: [`
+    i{
+      font-size: 48px;
+    }
+    small{
+      display: block;
+    }
+  `]
 })
 
 export class QuestionFormComponent {
+  iconos: Object[] = icons;
+
+  getIconVersion(icon: any) {
+    let version;
+    if (icon.versions.font.includes('plai-wordmark')) {
+      version = 'plain-wordmark';
+    } else {
+      version = icon.versions.font[0];
+    }
+    return version;
+  }
+
   onSubmit(form: NgForm) {
     const q = new Question(
       form.value.title,
       form.value.description,
-      new Date()
+      new Date(),
+      form.value.icon
     );
     console.log(q);
   }
